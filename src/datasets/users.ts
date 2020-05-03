7494891966da4478b6e43eca55aa6f5099b6fd85
labarruffe <lucas_barruffe@hotmail.com>
2020-05-03 15:18:12 -0300

    Create users datesets

---
+++ src/datasets/users.ts
@@ -0,0 +1,12 @@
+module.exports = (`
+    insert into users (name, email, password) values ('Eolande Boothebie', 'eboothebie0@typepad.com', 'PFLNYLwyUbft');
+    insert into users (name, email, password) values ('Clarice Housby', 'chousby1@baidu.com', '2ReUM3');
+    insert into users (name, email, password) values ('Rockie De Zuani', 'rde2@cisco.com', 'Zh5mQmbOSj');
+    insert into users (name, email, password) values ('Guendolen Brittian', 'gbrittian3@istockphoto.com', 'OKKUkxxa7mx');
+    insert into users (name, email, password) values ('Brana Posvner', 'bposvner4@unesco.org', 'WFU6fAAa');
+    insert into users (name, email, password) values ('Dev Cartman', 'dcartman5@desdev.cn', 'DuICpMwK');
+    insert into users (name, email, password) values ('Candra MacAllester', 'cmacallester6@pen.io', 'rJaCQOFcan');
+    insert into users (name, email, password) values ('Rodney Mennithorp', 'rmennithorp7@pen.io', 'wR12JAA');
+    insert into users (name, email, password) values ('Evey Newbigging', 'enewbigging8@wunderground.com', 'kjbVNUV');
+    insert into users (name, email, password) values ('Libbey Jarman', 'ljarman9@walmart.com', 'URgmiWJ8');
+`)
