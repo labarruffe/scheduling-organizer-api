3b41c0cd125d0567baa0625effe9063fc9890ef2
labarruffe <lucas_barruffe@hotmail.com>
2020-05-03 15:18:50 -0300

    Create customer dataset

---
+++ src/datasets/customers.ts
@@ -0,0 +1,6 @@
+module.exports = (`
+    insert into customer (id) values (1);
+    insert into customer (id) values (2);
+    insert into customer (id) values (3);
+    insert into customer (id) values (4);
+`)
