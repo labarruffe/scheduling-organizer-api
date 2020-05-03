80100e1babc050479d05fe0fdaf8b33efdad9dbb
labarruffe <lucas_barruffe@hotmail.com>
2020-05-03 15:19:09 -0300

    Create entertainer dataset

---
+++ src/datasets/entertainers.ts
@@ -0,0 +1,8 @@
+module.exports = (`
+    insert into entertainer (id, category, shiftsavailable, daysofweekavailable, price) values (5, 'JUGGLER', '{MORNING, AFTERNOON, NIGHT}', '{TUESDAY, WEDNESDAY, THURSDAY}', 170.08);
+    insert into entertainer (id, category, shiftsavailable, daysofweekavailable, price) values (6, 'CLOWN', '{NIGHT}', '{MONDAY}', 103.86);
+    insert into entertainer (id, category, shiftsavailable, daysofweekavailable, price) values (7, 'JUGGLER', '{MORNING, AFTERNOON}', '{MONDAY}', 184.26);
+    insert into entertainer (id, category, shiftsavailable, daysofweekavailable, price) values (8, 'CLOWN', '{MORNING, AFTERNOON, NIGHT}', '{FRIDAY, SATURDAY, SUNDAY}', 61.82);
+    insert into entertainer (id, category, shiftsavailable, daysofweekavailable, price) values (9, 'CLOWN', '{MORNING, AFTERNOON}', '{TUESDAY, WEDNESDAY, THURSDAY}', 176.72);
+    insert into entertainer (id, category, shiftsavailable, daysofweekavailable, price) values (10, 'MAGICIAN', '{MORNING, AFTERNOON, NIGHT}', '{TUESDAY, WEDNESDAY, THURSDAY}', 77.07);
+`)
