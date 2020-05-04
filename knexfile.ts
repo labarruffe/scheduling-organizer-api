d8481151a5eb7165a7ffea015c37091e5ab93079
labarruffe <lucas_barruffe@hotmail.com>
2020-05-03 23:35:09 -0300

    Sort seeds executions

--- knexfile.ts
+++ knexfile.ts
@@ -2,5 +2,11 @@ require('dotenv').config()
 
 module.exports = {
     client: process.env.DATABASE_CLIENT,
-    connection: process.env.DATABASE_CONNECTION_STRING
+    connection: process.env.DATABASE_CONNECTION_STRING,
+    migrations: {
+        directory: __dirname + '/migrations'
+    },
+    seeds: {
+        directory: __dirname + '/seeds'
+    }
 };
